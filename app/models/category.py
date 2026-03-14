13f6424009c6d08571bf34be2f1e43e2b9adc562
Truong Nguyen <truong@gmail.com>
2026-03-14 13:18:54 +0700

    Restructure project: separate backend and frontend

--- app/models/category.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class CategoryCreate(BaseModel):
-    name:str
-class CategoryResponse(CategoryCreate):
-    id:str
--- app/routers/admin/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.routers.admin.auth import router as auth_router
-from app.routers.admin.place_admin_router import router as place_router
-from app.routers.admin.category_admin_router import router as category_router
-router = APIRouter(prefix="/api")
-router.include_router(auth_router,prefix="/admin")
-router.include_router(place_router,prefix="/admin")
-router.include_router(category_router,prefix="/admin")

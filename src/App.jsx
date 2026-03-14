f4cd48be377c227a56d5274ba64b6e653e6e12d6
Truong Nguyen <truong@gmail.com>
2026-03-14 13:27:05 +0700

    Restructure project separate backend and frontend

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home/>} />
-        <Route path="/forgot" element={<ForgotPassword/>}></Route>
+        <Route path="/" element={<Home />} />   {/* thêm dòng này */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/forgot" element={<ForgotPassword />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
